6ea8144a6261ec70a4e1b3b0f6a7fb7301f532c7
Goran Kostadinov <goran.kostadinov@ge.com>
2016-08-19 18:27:34 +0300

    qtbuild trying to see error where building from command line

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
-int main(int argc, char *argv[])
+int main(int /*argc*/, char **/*argv*/)
 {
-    std::cout << "Start googletest application"
+    std::cout << "Start googletest application";
     return 1;
 }
